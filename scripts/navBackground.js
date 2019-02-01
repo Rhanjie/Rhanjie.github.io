32614ff6d67470fe534ef79c5d23baea270138a3
RhAnjiE <irhanjie@gmail.com>
2019-02-01 01:29:09 +0100

    Improved nav panel

---
+++ scripts/navBackground.js
@@ -0,0 +1,15 @@
+/*element = document.querySelector("#alwaysOnTop");
+
+var setBackgroundColor = function() {
+    var y = window.scrollY;
+    
+    if (y > 200) {
+        element.style.backgroundColor = "rgb(68, 68, 68)";
+        element.style.borderColor = "rgb(68, 68, 68)";
+        element.style.border
+    }
+
+    else element.style.backgroundColor = "#181818";
+}
+
+window.addEventListener("scroll", setBackgroundColor);*/
